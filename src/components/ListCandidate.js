a39b168d01f4c6eec93ee895a81190ef2e930359
KShraddha-angularminds <shraddha.karpe@angularminds.in>
2022-05-19 18:34:49 +0530

    00

--- src/components/ListCandidate.js
+++ src/components/ListCandidate.js
@@ -1,55 +1,67 @@
-import React from 'react'
+import React from "react";
 
 function ListCandidate() {
-    const storageData =JSON.parse(localStorage.getItem("data"))
-    console.log(storageData)
+  const storageData = JSON.parse(localStorage.getItem("data"));
+  console.log(storageData);
+  const sum = 0;
   return (
-    <div><body class="bg-light">
-
-    <div class="container my-4">
-        <main>
+    <div>
+      <body class="bg-light">
+        <div class="container my-4">
+          <main>
             <div class="py-5">
-                <h2>
-                    Candidates List
-                    <button class="btn btn-primary float-end">Add Candidate</button>
-                </h2>
+              <h2>
+                Candidates List
+                <button class="btn btn-primary float-end">Add Candidate</button>
+              </h2>
             </div>
 
             <div class="row">
-                <div class="col-12 ms-auto me-auto">
-                    <div class="card">
-                        <div class="card-body">
-                            <table class="table">
-                                <tr>
-                                    <th>#</th>
-                                    <th>Name</th>
-                                    <th>Email</th>
-                                    <th>Number of Skills</th>
-                                    <th>Total Work Experience (in months)</th>
-                                    <th>Actions</th>
-                                </tr>
-                                <tr>
-                                    <td>1</td>
-                                    <td>{storageData.fname}</td>
-                                    <td>{storageData.email}</td>
-                                    <td>{storageData.languages.length}</td>
-                                    <td>2</td>
-                                    <td>
-                                        <a href="#">Edit</a>
-                                        <a href="#" class="text-danger ms-2">Delete</a>
-                                    </td>
-                                </tr>
-                               
-                            </table>
-                        </div>
-                    </div>
+              <div class="col-12 ms-auto me-auto">
+                <div class="card">
+                  <div class="card-body">
+                    <table class="table">
+                      <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Number of Skills</th>
+                        <th>Total Work Experience (in months)</th>
+                        <th>Actions</th>
+                      </tr>
+                      {storageData.map((data, id) => {
+                        return (
+                          <tr>
+                            <td>{id + 1}</td>
+                            <td>{data.fname + "" + data.lname}</td>
+                            <td>{data.email}</td>
+                            <td>{data.languages.length}</td>
+                            <td>
+                              {data.experience.map((v, i) => {
+                                v.map((val, index) => {
+                                  return sum + val.duration;
+                                });
+                              })}
+                            </td>
+                            <td>
+                              <a href="#">Edit</a>
+                              <a href="#" class="text-danger ms-2">
+                                Delete
+                              </a>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </table>
+                  </div>
                 </div>
+              </div>
             </div>
-        </main>
+          </main>
+        </div>
+      </body>
     </div>
-</body>
-</div>
-  )
+  );
 }
 
-export default ListCandidate
+export default ListCandidate;
